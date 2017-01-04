e2e2544924a9e5a634926b26717f1f0088a6b6b9
Conrad De Peuter <conrad.depeuter@gmail.com>
2017-01-04 17:39:27 -0500

    cleanup

--- shotlinkToWeather.R
+++ shotlinkToWeather.R
@@ -15,6 +15,8 @@ allFiles <- list.files(shotlink.directory)
 for(f in allFiles){
     if(length(grep(".txt", f)) > 0){
         
+
+        
         
         #get all shots
         shots <- read.csv(paste0(shotlink.directory,"/", f), sep=";", header=TRUE)
@@ -29,37 +31,36 @@ for(f in allFiles){
         #get tournament for this event by matching the date of the first shot
         thisTournament <- events.us[which( as.Date(events.us$start) == as.Date(shots.first$Date, format="%m/%d/%Y")),]
         
-        #get all weather for shots in this file
-        weather <- getAllWeatherForShots(shots, thisTournament)
         
-        
-        #get weather data for each shot
-        shots.conditions <- apply(shots, 1, getWeatherForShot, weather)
-        shots.weather <- do.call("rbind", shots.conditions)
-        shots.weather$shotId <- shotIds
+        # make sure zip is 5 chars, TODO fix globally
+        zipCode <- thisTournament$zipCode
+        if(nchar(as.character(zipCode)) == 4){
+            #if leading 0 is removed we want to add one
+            zipCode <- paste0("0", as.character(zipCode))
+        }
         
         
         #write data to files
         filePrefix <- paste0("./data/shotlink/processed/shotlink-",gsub(" ", "_", thisTournament$label),"-",season)
-        weatherFile <- paste0( filePrefix,"-",thisTournament$zipCode,"-weather",".csv")
+        weatherFile <- paste0( filePrefix,"-",zipCode,"-weather",".csv")
         shotFile <- paste0(filePrefix,".csv")
-        write.table(shots.weather, weatherFile, sep=",", row.names = FALSE)
-        write.table(shots, shotFile, sep = ",", row.names = FALSE)
+        
+        #if this tournaments already been processed dont do this again
+        if(!file.exists(weatherFile) && !file.exists(shotFile)){
+            #get all weather for shots in this file
+            weather <- getAllWeatherForShots(shots, thisTournament)
+            
+            
+            #get weather data for each shot
+            shots.conditions <- apply(shots, 1, getWeatherForShot, weather)
+            shots.weather <- do.call("rbind", shots.conditions)
+            shots.weather$shotId <- shotIds
+            
+            
+     
+            
+            write.table(shots.weather, weatherFile, sep=",", row.names = FALSE)
+            write.table(shots, shotFile, sep = ",", row.names = FALSE) 
+        }
     }
 }
-
-
-
-
-
-
-
-# 
-# 
-# #day never teed off on third day, check stricker
-# stricker <- shots[shots$Player.. == 6527,]
-# stricker.conditions <- apply(stricker, 1, getWeatherForShot, weather)
-# stricker.Df <- do.call("rbind", stricker.conditions)
-# stricker.Df$id <- stricker$shotId
-#     
-# write.table(stricker.Df, "./data/shot-2016-stricker-weather-pga.csv", sep=",", row.names = FALSE)
