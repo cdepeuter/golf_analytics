a4027f300123858c8e2969b463fa736e7aaaa091
Conrad De Peuter <conrad.depeuter@gmail.com>
2017-03-04 15:39:04 -0500

    update

--- R/main_scrape_proviz.R
+++ R/main_scrape_proviz.R
@@ -6,8 +6,23 @@ main_scrape_proviz <- function(){
     locTable <- do.call(rbind, pgalocation)
     
     pga.allcourses <- unique(pga$course.1)
+    pga.ids <- unlist(lapply(pga.allcourses, getCourseId, pga))
+    print(pga.ids)
+    print(length(pga.ids))
     dfs <- lapply(pga.allcourses, getCleanProvizForCourse)
     df <- do.call("rbind", dfs)
-    
+    df <- cbind(df, pga.ids)
     return(df)
+}
+
+
+getCourseId <- function( courseName, events){
+    id <- events[events[["course.1"]] == courseName, "course"]
+    print(courseName)
+    print(id)
+    return(id[[1]])
+}
+
+add_id_to_proviz <- function(){
+    proviz <- read.csv("./data/pga-hole-coords.csv", header=TRUE)
 }
